d4b9e609353c002c9f2314c26cc976cdfeaf301e
FriendlyMaabuat <friendlymaabuat25@gmail.com>
2024-05-07 13:05:02 +0800

    Sending an email when conditions are met, with the program being ran locally and continuously

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+from email.mime.text import MIMEText
+
 import requests
 import selectorlib
+import smtplib, ssl
+import os
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
@@ -14,7 +19,23 @@ def extract(source):
     value = extractor.extract(source)["tours"]
     return value
 
-def send_email():
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "myudemycourses47@gmail.com"
+    password = "irjhfowavapmbhuk"
+
+    message = MIMEText(message, "plain")
+    message["Subject"] = "MusicEventWebScraper Subject Test Email"
+    message["From"] = username
+
+    receiver = "myudemycourses47@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message.as_string())
     print("Email was sent!")
 
 def store(extracted):
@@ -26,14 +47,19 @@ def read(extracted):
         return file.read()
 
 if __name__ == "__main__":
-    scraped = scrape(URL)
-    extracted = extract(scraped)
-    print(extracted)
-    content = read(extracted)
-    if extracted != "No upcoming tours":
-        if extracted not in content:
-            store(extracted)
-            send_email()
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+        content = read(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                message = """
+                If you see this, then the email was successfully sent!
+                """
+                send_email(message)
+        time.sleep(2)  # Checking for new tours every 2 seconds
 
 
 
